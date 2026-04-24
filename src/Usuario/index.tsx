5590c29e0c0fdf5c7d24f187e457e9c5160ba00d
4ndriu <andres.castillop@uniagustiniana.edu.co>
2026-04-24 14:30:04 +0000

    cambios style

--- src/Usuario/index.tsx
+++ src/Usuario/index.tsx
@@ -1,15 +1,18 @@
+import "./style.css"
 
-function Usuario(){
-    return(
-        <>
-            <h1>Perfil</h1>
-            <h2>Información del usuario</h2>
-            <p>Nombre: Juan Pérez</p>
-            <p>Email: juan.perez@ejemplo.com</p>
-            <p>Pais: Venezuela</p>
-            <p>Edad: 30 años</p>
-        </>
-    )
+function Usuario() {
+  return (
+    <div className="usuario-container">
+      <h1>Perfil</h1>
+      <div className="usuario-card">
+        <h2>Información del usuario</h2>
+        <p><span>Nombre:</span> Juan Pérez</p>
+        <p><span>Email:</span> juan.perez@ejemplo.com</p>
+        <p><span>País:</span> Venezuela</p>
+        <p><span>Edad:</span> 30 años</p>
+      </div>
+    </div>
+  )
 }
 
 export default Usuario
